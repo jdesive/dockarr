f02c16cd29fd2a79ddeea3c3de467c45eca9d516
Jack DeSive <jackdesive17@gmail.com>
2022-12-24 20:14:02 -0600

    Add env file and start/update scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Setting configuration environment variables"
 export "$(xargs < .env)"
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 docker compose down
 git fetch --all
