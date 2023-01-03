3918da97fdddc8d48a446fc77d8002d44e83a179
Jack DeSive <jackdesive17@gmail.com>
2023-01-02 19:25:38 -0600

    Add missing env vars to start.sh script

--- start.sh
+++ start.sh
@@ -14,6 +14,9 @@ export HTPC_VPN_NETWORK="192.168.1.0/24"
 export HTPC_PLEX_CLAIM=""
 export HTPC_VPN_PROV="pia"
 export HTPC_VPN_CLIENT="openvpn"
+export HTPC_UPDATE_CRON="0 3 * * 6"
+export HTPC_DOCKER_SOCK="/var/run/docker.sock"
+export HTPC_UPDATE_CLEANUP="true"
 echo "Configuration complete"
 
 docker compose up -d
